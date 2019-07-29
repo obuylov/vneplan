af82798e5fd2251a3cc3e9fec61aa48728afc024
Oleg <bom-1990@yandex.ru>
2019-07-29 19:15:51 +0300

    Внесены правки

--- src/extensions/adapt-tooltip/js/adapt-tooltip.js
+++ src/extensions/adapt-tooltip/js/adapt-tooltip.js
@@ -2,95 +2,102 @@
  * adapt-tooltip
  * License -
  */
-define(function(require) {
-    var Adapt = require('core/js/adapt');
-    var CONFIG = require('./conf');
-    var $tooltip = null;
-    var $targetEle = null;
+define(function (require) {
+  var Adapt = require('core/js/adapt');
+  var CONFIG = require('./conf');
+  var $tooltip = null;
+  var $targetEle = null;
 
-    function setUptoolTip() {
-        window.tooltip = onMouseEnter;
-    };
+  function setUptoolTip() {
+    window.tooltip = onclick;
+  }
 
-    var onMouseEnter = function(evt) {
-        $tooltip = $('<div id="tooltip"></div>');
-        $targetEle = $(this);
-        var tip = $targetEle.attr('data-title');
-        if (!tip || tip == '') return false;
-        var lazyLayout = _.debounce(showTooltip, 200);
-        $tooltip.css('opacity', 0).html(tip).appendTo('body');
-        showTooltip();
-        $(window).resize(_.bind(lazyLayout, this));
-        $targetEle.on('mouseleave', removeTooltip);
-        $tooltip.on('click',removeTooltip);
-    };
+  var onclick = function (evt) {
+    $('.tooltip-window').remove();
+    $tooltip = $('<div class="tooltip-window"></div>');
+    $targetEle = $(this);
+    var tip = $targetEle.attr('data-title');
+    if (!tip || tip == '') return false;
+    //var lazyLayout = _.debounce(showTooltip, 200);
+    $tooltip.css('opacity', 0).html(tip).appendTo('body');
+    showTooltip();
 
-    function showTooltip() {
-        if (!$tooltip || !$targetEle) return;
-        setUptoolTipWidth();
-        var targetEleLeft = $targetEle.offset().left;
-        var targetEleTop = $targetEle.offset().top;
-        var targetEleWidth = $targetEle.outerWidth();
+    setTimeout(() => {$('body').on('click', removeTooltip); }, 500);
+    //$(window).resize(_.bind(lazyLayout, this));
+  };
 
-        var tooltipWidth = $tooltip.outerWidth();
-        var tooltipHeight = $tooltip.outerHeight();
+  function showTooltip() {
+    if (!$tooltip || !$targetEle) return;
+    setUptoolTipWidth();
+    var targetEleLeft = $targetEle.offset().left;
+    var targetEleTop = $targetEle.offset().top;
+    var targetEleWidth = $targetEle.outerWidth();
 
-        var posLeft = targetEleLeft + (targetEleWidth / 2) - (tooltipWidth / 2);
-        var posTop = targetEleTop - tooltipHeight - CONFIG.POSIONFROMTEXT;
+    var tooltipWidth = $tooltip.outerWidth();
+    var tooltipHeight = $tooltip.outerHeight();
 
-        if (posLeft < 0) {
-            posLeft = targetEleLeft + targetEleWidth / 2 - CONFIG.POSIONFROMTEXT;
-            $tooltip.addClass('left');
-        } else {
-            $tooltip.removeClass('left');
-        }
+    var posLeft = targetEleLeft + (targetEleWidth / 2) - (tooltipWidth / 2);
+    var posTop = targetEleTop - tooltipHeight - CONFIG.POSIONFROMTEXT;
 
-        if (posLeft + tooltipWidth > $(window).width()) {
-            posLeft = targetEleLeft - tooltipWidth + targetEleWidth / 2 + CONFIG.POSIONFROMTEXT;
-            $tooltip.addClass('right');
-        } else {
-            $tooltip.removeClass('right');
-        }
+    if (posLeft < 0) {
+      posLeft = targetEleLeft + targetEleWidth / 2 - CONFIG.POSIONFROMTEXT;
+      $tooltip.addClass('left');
+    } else {
+      $tooltip.removeClass('left');
+    }
 
-        if (posTop < 0) {
-            var posTop = targetEleTop + targetEleTop;
-            $tooltip.addClass('top');
-        } else {
-            $tooltip.removeClass('top');
-        }
+    if (posLeft + tooltipWidth > $(window).width()) {
+      posLeft = targetEleLeft - tooltipWidth + targetEleWidth / 2 + CONFIG.POSIONFROMTEXT;
+      $tooltip.addClass('right');
+    } else {
+      $tooltip.removeClass('right');
+    }
 
-        $tooltip.css({
-            left: posLeft,
-            top: posTop
-        }).animate({
-            top: '+=' + CONFIG.ANIMATE_TOP,
-            opacity: 1
-        }, CONFIG.DURATION, CONFIG.EASING);
+    if (posTop < 0) {
+      var posTop = targetEleTop + targetEleTop;
+      $tooltip.addClass('top');
+    } else {
+      $tooltip.removeClass('top');
+    }
 
-    };
-    function setUptoolTipWidth() {
-        if ($(window).width() < $tooltip.outerWidth() * CONFIG.PADDINGFACTOR) {
-            $tooltip.css('max-width', $(window).width() / 2);
-        } else {
-            $tooltip.css('max-width', CONFIG.MAX_WIDTH);
-        }
-    };
+    $tooltip.css({
+      left: posLeft,
+      top: posTop
+    }).animate({
+      top: '+=' + CONFIG.ANIMATE_TOP,
+      opacity: 1
+    }, CONFIG.DURATION, CONFIG.EASING);
 
-    function removeTooltip() {
-        $tooltip.animate({
-            top: '-=' + CONFIG.ANIMATE_TOP,
-            opacity: 0
-        }, CONFIG.DURATION, CONFIG.EASING, function() {
-            $(this).remove();
-            $tooltip = null;
-            $targetEle = null;
-        });
-    };
+  }
 
-    Adapt.on("app:dataReady", function() {
-        var tooltip = Adapt.course.get('_tooltip')
-        if (tooltip && tooltip._isEnabled) {
-            setUptoolTip();
-        }
+  function setUptoolTipWidth() {
+    if ($(window).width() < $tooltip.outerWidth() * CONFIG.PADDINGFACTOR) {
+      $tooltip.css('max-width', $(window).width() / 2);
+    } else {
+      $tooltip.css('max-width', CONFIG.MAX_WIDTH);
+    }
+  }
+
+  function removeTooltip() {
+    $('.tooltip-window').remove();
+    $('body').off('click', removeTooltip);
+  }
+
+  Adapt.on("app:dataReady", function () {
+    var tooltip = Adapt.course.get('_tooltip');
+    if (tooltip && tooltip._isEnabled) {
+      setUptoolTip();
+    }
+  });
+
+  Adapt.on("pageView:ready", function () {
+    $('.tooltip-window').remove();
+    $('span[data-rel=tooltip]').on('click', function () {
+      tooltip.call(this)
     });
+  });
+
+  Adapt.on('menuView:postRender', function () {
+    $('.tooltip-window').remove();
+  })
 });
